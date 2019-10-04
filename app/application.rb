63c17fe5ad201bd1f05a34523814c43c2afa5d87
Max Lee <maxolee23@gmail.com>
2019-10-04 22:28:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
     elsif req.path.match(/add/)
       item = req.params["item"]
       if @@items.include?(item)
-        @@cart << item 
+        @@cart << item
         resp.write "added #{item}"
       else
     else
